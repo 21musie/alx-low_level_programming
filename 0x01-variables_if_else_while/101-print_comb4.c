e68bb670d1a9608c8ae656f944487f30fe7b90d1
21musie <104737346+21musie@users.noreply.github.com>
2022-06-03 23:57:56 +0300

    GETTING ANGRY!!

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,45 +10,11 @@ int main(void)
 {
 	int a, b, c;
 	
-	for (a = '0'; a <= '7'; a++)
+	for (a = 48; a <= 55; a++)
 	{
+		for (b = a + 1; b <= 56; b++)
 		{
-			if (a == '0') 
-			b = '1';
-		if (a == '1')
-			b = '2';
-		if (a == '2')
-			b = '3';
-		if (a == '3')
-			b = '4';
-		if (a == '4')
-			b = '5';
-		if (a == '5')
-			b = '6';
-		if (a == '6')
-			b = '7';
-		if (a == '7')
-			b = '8';
-		}
-		for (; b <= '8'; b++)
-		{
-			if (b == '1')  
-				c = '2';
-			if (b == '2')
-				c = '3';
-			if (b == '3')
-				c = '4';
-			if (b == '4')
-				c = '5';
-			if (b == '5')
-				c = '6';
-			if (b == '6')
-				c = '7';
-			if (b == '7')
-				c = '8';
-			if (b == '8')
-				c = '9';			
-			for (; c <= '9'; c++)
+			for (c = b + 1; c <= 57; c++)
 			{
 				putchar(a);
 				putchar(b);
@@ -61,10 +27,9 @@ int main(void)
 				putchar(',');
 				putchar(' ');
 				}
-			}			
+			}
 		}
 	}
-
 	putchar('\n');
 
 	return (0);
