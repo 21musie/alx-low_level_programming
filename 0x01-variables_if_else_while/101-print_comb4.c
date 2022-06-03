3176139b149ea51444011024c9c09aa82186e311
21musie <104737346+21musie@users.noreply.github.com>
2022-06-03 21:00:22 +0300

    updating comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,20 +8,21 @@
  */
 int main(void)
 {
-	int a, b;
+	int a, b, c;
 
-	for (b = '0'; b <= '9'; b++)
+	for (a = '0'; a <= '7'; a++)
 	{
-		for (a = '0'; a <= '9'; a++)
+		for (b = '1'; b <= '8'; b++)
 		{
-			putchar(b);
-			putchar(a);
-			if (b != '9' || a != '9')
-			{
+			for (c = '2'; c = '9'; c++){
+				
+				putchar(a);
+				putchar(b);
+				putchar(c);
 				putchar(',');
 				putchar(' ');
+				
 			}
-
 		}
 	}
 
