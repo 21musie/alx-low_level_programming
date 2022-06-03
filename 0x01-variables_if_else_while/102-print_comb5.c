c082280a39f3c52369d2dd1e9bc7d81826aa8905
21musie <104737346+21musie@users.noreply.github.com>
2022-06-03 20:44:19 +0300

    just trying ...

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,24 +8,31 @@
  */
 int main(void)
 {
-	int a, b;
+	int a, b, c, d;
+	int i, j; /* counters */
 
-	for (b = '0'; b <= '9'; b++)
+	for (i = 0; i <= 99; i++)
 	{
-		for (a = '0'; a <= '9'; a++)
+		for (j = i + 1; j <= 99; j++)
 		{
-			putchar(b);
+			a = (i / 10) + '0';
+			b = (i % 10) + '0';
+			c = (j / 10) + '0';
+			d = (j % 10) + '0';
 			putchar(a);
-			if (b != '9' || a != '9')
+			putchar(b);
+			putchar(' ');
+			putchar(c);
+			putchar(d);
+
+			if (i != 98)
 			{
 				putchar(',');
 				putchar(' ');
 			}
-
 		}
 	}
 
 	putchar('\n');
-
 	return (0);
 }
