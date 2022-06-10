d679383322cdda48b6b66a778c0be67168bb0eae
21musie <104737346+21musie@users.noreply.github.com>
2022-06-11 02:28:12 +0300

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,19 +10,4 @@
 
 void print_number(int n)
 {
-  unsigned int m;
-
-if (n < 0)
-{
-_putchar('-');
-m = -n;
-}
-else
-{
-m = n;
-}
-if (m / 10 != 0)
-{
-print_number(m / 10);
-_putchar((m % 10) + '0');
 }
