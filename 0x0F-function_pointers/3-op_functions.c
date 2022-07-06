69e472b7e96a515d47686b2c674c17b2a67b4177
21musie <104737346+21musie@users.noreply.github.com>
2022-07-06 22:34:47 +0300

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,61 +1,71 @@
-#include <stdio.h>
+#include "3-calc.h"
 
 /**
- * op_add - This function returns the sum of a and b
- * @a: The first int to add
- * @b: The second int to add
- * Return: The integer sum of both a and b
+ * op_add - adds two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: add.
  */
-
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
- * op_sub - This function returns the difference of a and b
- * @a: The first difference operator
- * @b: The second difference operator
- * Return: The difference between a and b
+ * op_sub - subctracts two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: difference.
  */
-
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
 /**
- * op_mul - This function returns the product of a and b
- * @a: The first int to multiply
- * @b: The second int to multiply
- * Return: The product of both a and b
+ * op_mul - multiplies two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: multiplication.
  */
-
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
 /**
- * op_div - This function returns the result of the division of a by b
- * @a: The int dividend
- * @b: The int divisor
- * Return: The quotient without remainder of a divided by b
+ * op_div - divides two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: division.
  */
-
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
 
 /**
- * op_mod - This function returns the remainder of the division of a by b
- * @a: The int dividend
- * @b: The int divisor
- * Return: The remainder from the quotient of a divided by b
+ * op_mod - calculates the module of two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: remainder of the division.
  */
-
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
